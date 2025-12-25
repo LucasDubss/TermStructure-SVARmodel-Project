e2069025eb7808d65ed5fc06d5e27569c13b9008
Lucas Dubois <l.dubois@student.uliege.be>
2025-12-25 21:11:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<div style="text-align: center;">
+  <h1>Term-Structure and SVAR Monetary Policy Model in Germany</h1>
+  <h3>HEC Liege</h3>
+  <h4><em>Lucas Dubois and Myriam Lamborelle</em></h4> 
+
+
+---
+
+##  About
+This repository contains the codes for a project for the course of **Macroeconomics and Finance** at *HEC Liege*. It is divided into two different parts:
+
+- A Term-Structure Model for German Bonds.
+- A SVAR analysis on German Monetary Policy.
+
+---
+
+
+
+
---
+++ termstructure.R
@@ -0,0 +1,11 @@
+#========================================================================================
+#                                                                     T E R M  -  S T R U C T U  R E     C O D E 
+#                                                                        Lucas Dubois & Myriam  Lamborelle
+#========================================================================================
+
+
+#1.0  Libraries:
+library(readsdmx)
+library(tidyverse)
+library(lubridate)
+library(zoo)
